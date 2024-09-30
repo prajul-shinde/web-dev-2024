ef8252bf05661a8f8bf358d91acdfc38ff577877
prajul-shinde <prajulshinde@gmail.com>
2024-09-30 17:18:09 +0530

    condtional statements

---
+++ js-foundation/part3/conditional-challenges.js
@@ -0,0 +1,27 @@
+// check if number is greater than another number
+let num1 = 5;
+let num2 = 7;
+
+if (num1 > num2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//check if string equal to another string
+
+let str1 = "prajul";
+let str2 = "prajul";
+if (str1 == str2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//check if variable is number or not
+let num = "123";
+if (typeof num == "number") {
+  console.log("vraible is a number");
+} else {
+  console.log("variable is not a number");
+}
